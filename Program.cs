4094d64eee6a29e0d5ad538824c4a3f85c0dbee6
Pablo-Castillo <pablo.castillo01@alumnos.ucn.cl>
2020-05-18 20:55:09 -0400

    Feat (Program.cs): Implemented the comunication interface

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace fivet.server
+
+namespace Fivet.ZeroIce
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using(Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                {
+                    var adapter =
+                        communicator.createObjectAdapterWithEndpoints("TheSystemAdapter", "default -h localhost -p 10000");
+                    adapter.add(new TheSystemImpl(), Ice.Util.stringToIdentity("cl.ucn.disc.pdis.fivet.zeroice.model.TheSystem"));
+                    adapter.activate();
+                    communicator.waitForShutdown();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(e); 
+                return 1;               
+            } 
+            return 0;           
+        }
+    }
+
+    class TheSystemImpl : model.TheSystemDisp_
+    {
+        public override long getDelay(long clientTime, Ice.Current current = null){
+            return DateTime.Now.Ticks - clientTime;
         }
     }
 }
